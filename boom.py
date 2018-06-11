5c7f4da929f5b1a34f77ec365250ff2b362636d1
Taku Tanichi <sina.1868@gmail.com>
2018-06-11 23:21:30 +0900

    test

--- boom.py
+++ boom.py
@@ -7,6 +7,8 @@ import chainer.links as L
 from chainer import training
 from chainer.training import extensions
 
+import matplotlib
+matplotlib.use('Agg')
 
 # Network definition
 class MLP(chainer.Chain):
@@ -27,22 +29,14 @@ class MLP(chainer.Chain):
 
 def main():
     parser = argparse.ArgumentParser(description='Chainer example: MNIST')
-    parser.add_argument('--batchsize', '-b', type=int, default=100,
-                        help='Number of images in each mini-batch')
-    parser.add_argument('--epoch', '-e', type=int, default=20,
-                        help='Number of sweeps over the dataset to train')
-    parser.add_argument('--frequency', '-f', type=int, default=-1,
-                        help='Frequency of taking a snapshot')
-    parser.add_argument('--gpu', '-g', type=int, default=-1,
-                        help='GPU ID (negative value indicates CPU)')
-    parser.add_argument('--out', '-o', default='result',
-                        help='Directory to output the result')
-    parser.add_argument('--resume', '-r', default='',
-                        help='Resume the training from snapshot')
-    parser.add_argument('--unit', '-u', type=int, default=1000,
-                        help='Number of units')
-    parser.add_argument('--noplot', dest='plot', action='store_false',
-                        help='Disable PlotReport extension')
+    parser.add_argument('--batchsize', '-b', type=int, default=100, help='Number of images in each mini-batch')
+    parser.add_argument('--epoch', '-e', type=int, default=20, help='Number of sweeps over the dataset to train')
+    parser.add_argument('--frequency', '-f', type=int, default=-1, help='Frequency of taking a snapshot')
+    parser.add_argument('--gpu', '-g', type=int, default=-1, help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--out', '-o', default='result', help='Directory to output the result')
+    parser.add_argument('--resume', '-r', default='', help='Resume the training from snapshot')
+    parser.add_argument('--unit', '-u', type=int, default=1000, help='Number of units')
+    parser.add_argument('--noplot', dest='plot', action='store_false', help='Disable PlotReport extension')
     args = parser.parse_args()
 
     print('GPU: {}'.format(args.gpu))
@@ -68,12 +62,10 @@ def main():
     train, test = chainer.datasets.get_mnist()
 
     train_iter = chainer.iterators.SerialIterator(train, args.batchsize)
-    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,
-                                                 repeat=False, shuffle=False)
+    test_iter = chainer.iterators.SerialIterator(test, args.batchsize,repeat=False, shuffle=False)
 
     # Set up a trainer
-    updater = training.updaters.StandardUpdater(
-        train_iter, optimizer, device=args.gpu)
+    updater = training.updaters.StandardUpdater(train_iter, optimizer, device=args.gpu)
     trainer = training.Trainer(updater, (args.epoch, 'epoch'), out=args.out)
 
     # Evaluate the model with the test dataset for each epoch
@@ -93,12 +85,9 @@ def main():
     # Save two plot images to the result dir
     if args.plot and extensions.PlotReport.available():
         trainer.extend(
-            extensions.PlotReport(['main/loss', 'validation/main/loss'],
-                                  'epoch', file_name='loss.png'))
+            extensions.PlotReport(['main/loss', 'validation/main/loss'],'epoch', file_name='loss.png'))
         trainer.extend(
-            extensions.PlotReport(
-                ['main/accuracy', 'validation/main/accuracy'],
-                'epoch', file_name='accuracy.png'))
+            extensions.PlotReport(['main/accuracy', 'validation/main/accuracy'],'epoch', file_name='accuracy.png'))
 
     # Print selected entries of the log to stdout
     # Here "main" refers to the target link of the "main" optimizer again, and
